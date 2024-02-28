89e7d75c3059867753f945c5af8d1dbb646215e9
Uzurch1k <galcenkovlad7@gmail.com>
2024-02-28 22:28:54 +0200

    fix js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 // ==============================================================
 
-// ==============================================================
+// import { etchApi } from './js/fetch-api';
+// import { localStorage } from './js/local-storage';
+// import { renderingBooks } from './js/rendering-books';
 
-// ==============================================================
+// import { header } from './js/header';
+// import { authorization } from './js/authorization';
+
+// import { categories } from './js/categories';
+// import { support } from './js/support';
+
+// import { books } from './js/books';
+// import { shopping } from './js/shopping';
+// import { popup } from './js/popup';
 
 // ==============================================================
