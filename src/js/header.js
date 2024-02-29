6076978cbaefc91353578fb23970b224121f4c5b
ViktoriiaYakaba <yakaba@bk.ru>
2024-02-29 19:27:12 +0100

    summary

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,19 @@
-// ==============================================================
+const modalBtnOpen= document.querySelector(".header-btn-burgher")
+const modalBtnClose = document.querySelector('.header-btn-esc');
+const modal = document.querySelector('.header-menu');
 
-// ==============================================================
 
-// ==============================================================
+modalBtnOpen.addEventListener('click', function () {
+    modalBtnOpen.classList.toggle('active');
+    modal.classList.toggle('active');
+    modal.classList.toggle('hidden');
+    modalBtnClose.classList.toggle('hidden');
+});
 
-// ==============================================================
+modalBtnClose.addEventListener('click', function () {
+    modalBtnOpen.classList.toggle('active');
+    modal.classList.toggle('active');
+    modal.classList.toggle('hidden');
+    modalBtnClose.classList.toggle('hidden');
+});
+
