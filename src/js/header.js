27fefe53f973a508c54c5fc0e10024af9453c6a0
ViktoriiaYakaba <yakaba@bk.ru>
2024-02-29 19:54:44 +0100

    add button modal

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,15 @@
-// ==============================================================
 
-// ==============================================================
+modalBtnOpen.addEventListener('click', function () {
+    modalBtnOpen.classList.toggle('hidden');
+    modal.classList.toggle('active');
+    modal.classList.toggle('hidden');
+    modalBtnClose.classList.toggle('active');
+});
 
-// ==============================================================
-
-// ==============================================================
+modalBtnClose.addEventListener('click', function () {
+    modalBtnOpen.classList.toggle('hidden');
+    modal.classList.toggle('active');
+    modal.classList.toggle('hidden');
+    modalBtnClose.classList.toggle('active');
+});
+i
