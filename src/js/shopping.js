1b5c098fd17ebb972adb9c365e86fdeeb911595e
Marensfc <alexeymaren12@gmail.com>
2024-03-01 23:07:52 +0200

    Add icons to page numbers

--- src/js/shopping.js
+++ src/js/shopping.js
@@ -6,6 +6,7 @@
 
 import Pagination from 'tui-pagination';
 // import 'tui-pagination/dist/tui-pagination.css';
+import '../css/shopping.css';
 
 export const pagesContainer = document.querySelector('#pagination-wrapper');
 
@@ -13,17 +14,18 @@ const instance = new Pagination(pagesContainer, {
   totalItems: 10,
   itemsPerPage: 3,
   visiblePages: 3,
+  centerAlign: true,
   template: {
     page: '<a href="#" class="tui-page-btn">{{page}}p</a>',
     currentPage:
       '<strong class="tui-page-btn tui-is-selected">{{page}}p</strong>',
     moveButton:
       '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}"></span>' +
       '</a>',
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}">next</span>' +
       '</span>',
     moreButton:
       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
@@ -31,5 +33,7 @@ const instance = new Pagination(pagesContainer, {
       '</a>',
   },
 });
+
+console.log(instance);
 
 // ==============================================================
