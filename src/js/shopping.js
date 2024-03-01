2a72fdc0122c5bfe58bb74d76977a5d4f16a3c71
Marensfc <alexeymaren12@gmail.com>
2024-03-01 14:22:13 +0200

    Add page numbers library

--- src/js/shopping.js
+++ src/js/shopping.js
@@ -4,4 +4,34 @@
 
 // ==============================================================
 
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+export const pagesContainer = document.querySelector('#pagination-wrapper');
+
+const instance = new Pagination(pagesContainer, {
+  totalItems: 10,
+  itemsPerPage: 3,
+  visiblePages: 3,
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}p</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}p</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+});
+
+
+
 // ==============================================================
