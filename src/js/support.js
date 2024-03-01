bb8f979126d558af3517465a835260460ac57d16
Svitlana923 <svitlanakostiuk11@gmail.com>
2024-03-01 23:03:19 +0100

    1

---
+++ src/js/support.js
@@ -0,0 +1,32 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+    loop: false,
+  speed: 100,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 6,
+            spaceBetween: 10
+        },
+    },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    },
+    mousewheel: {
+    invert: true,
+    },
+    watchOverflow: true,
+    shortSwipes: false,
+//     navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+});
