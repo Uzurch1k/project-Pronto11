5f8bc7f2084873e144e404548d54db4c962f3b3b
Svitlana923 <svitlanakostiuk11@gmail.com>
2024-03-02 12:52:32 +0100

    2

--- src/js/support.js
+++ src/js/support.js
@@ -1,60 +1,24 @@
-<<<<<<< Updated upstream
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 
 
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'vertical',
     loop: false,
   speed: 100,
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 6,
-            spaceBetween: 10
-        },
-    },
+  slidesPerView: 6,
   keyboard: {
     enabled: true,
-    onlyInViewport: false,
+    onlyInViewport: true,
     },
-    mousewheel: {
+  mousewheel: {
     invert: true,
     },
-    watchOverflow: true,
-    shortSwipes: false,
-//     navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
+  shortSwipes: false,
+ navigation: {
+nextEl: '.swiper-button-next',
+prevEl: '.swiper-button-prev',
+},
 
 });
-=======
-
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
-
-const swiper = new Swiper('.swiper', {
-
-  direction: 'vertical',
-    loop: true,
-    speed: 400,
-    spaceBetween: 0,
-  centeredSlides: true,
-
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-export function openCharitySite(url) {
-        window.open(url, '_blank');
-    }
->>>>>>> Stashed changes
