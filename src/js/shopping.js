d4a603ee57d14e0f68af6b8ee52bb1d186656e04
Marensfc <alexeymaren12@gmail.com>
2024-03-02 23:51:56 +0200

    Page-number-css, made mobyle styles

--- src/js/shopping.js
+++ src/js/shopping.js
@@ -14,18 +14,18 @@ const options = {
   totalItems: 10,
   itemsPerPage: 3,
   visiblePages: 2,
-  centerAlign: true,
+  centerAlign: false,
   template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}p</a>',
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
     currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}p</strong>',
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
     moveButton:
       '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}"></span>' +
       '</a>',
     disabledMoveButton:
       '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '<span class="tui-ico-{{type}}"></span>' +
       '</span>',
     moreButton:
       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
