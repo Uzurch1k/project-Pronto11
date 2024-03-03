fea506593eb2c439f21792ec5dc335c622463a83
AnnaHryshaieva <anet01073@gmail.com>
2024-03-04 00:22:01 +0200

    done

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,5 @@
 import { fetchBookById } from './fetch-api';
+import { saveBookToLocalStorage } from './local-storage';
 
 // ==============================================================
 // функція спрацьовує при кліку на QUICK VIEW
@@ -11,14 +12,16 @@ setTimeout(() => {
 export async function popup() {
   const booksList = document.querySelector('.books-container');
 
-  booksList.addEventListener('click', onBookLinkClick);
+  if (booksList) {
+    booksList.addEventListener('click', onBookLinkClick);
+  }
 
   async function onBookLinkClick(event) {
-    const popupBookWrapper = document.querySelector('.popup-book');
+    const popupBookWrapper = document.querySelector('.popup-window');
 
     popupBookWrapper.innerHTML = '';
 
-    if (event.target === event.target.closest('.books-overlay')) {
+    if (event.target.classList.contains('books-overlay')) {
       const bookId = event.target.dataset.id;
       const bookData = await fetchBookById(bookId);
 
@@ -40,10 +43,19 @@ async function openModal() {
   const closeBtn = document.querySelector('.popup-close-btn');
 
   showModal();
+  checkLocalStorage();
+  clickOnAddToShopingListBtn();
 
   backdrop.addEventListener('click', onBackdropClick);
   document.addEventListener('keydown', onEscClick);
   closeBtn.addEventListener('click', onClickCloseBtn);
+  document.addEventListener('keydown', onSpaceButtonClick);
+
+  function onSpaceButtonClick(event) {
+    if (event.keyCode === 32) {
+      event.preventDefault();
+    }
+  }
 
   function showModal() {
     backdrop.classList.add('is-active');
@@ -52,12 +64,14 @@ async function openModal() {
   function onClickCloseBtn() {
     hideModal();
     closeBtn.removeEventListener('click', onClickCloseBtn);
+    document.removeEventListener('keydown', onSpaceButtonClick);
   }
 
   function onBackdropClick(event) {
     if (event.target === event.currentTarget) {
       hideModal();
       backdrop.removeEventListener('click', onBackdropClick);
+      document.removeEventListener('keydown', onSpaceButtonClick);
     }
   }
 
@@ -65,6 +79,7 @@ async function openModal() {
     if (event.keyCode === 27) {
       hideModal();
       document.removeEventListener('keydown', onEscClick);
+      document.removeEventListener('keydown', onSpaceButtonClick);
     }
   }
 
@@ -77,23 +92,23 @@ async function openModal() {
 // шаблон динамічної частини модалки
 // ==============================================================
 function template(obj) {
-  const { author, title, book_image, buy_links, description = '' } = obj;
+  const { _id, author, title, book_image, buy_links, description = '' } = obj;
   const [amazon, appleBook] = buy_links;
 
   return `
+<button class="popup-close-btn">
+      <svg class="popup-close-btn-icon" width="28" height="28">
+        <use href="./img/icons.svg#icon-header-close"></use>
+      </svg>
+    </button>
+    <div class="popup-book">
       <div class="popup-wrap-img">
-        <img
-          class="popup-book-image"
-          src="${book_image}"
-          alt="обкладинка"
-        />
+        <img class="popup-book-image" src="${book_image}" alt="обкладинка" />
       </div>
       <div class="popup-book-content">
         <h2 class="popup-book-title">${title}</h2>
         <h3 class="popup-book-author">${author}</h3>
-        <p class="popup-book-review">
-          ${description}
-        </p>
+        <p class="popup-book-review">${description}</p>
         <ul class="popup-shopping-links">
           <li>
             <a
@@ -128,21 +143,78 @@ function template(obj) {
           </li>
         </ul>
       </div>
-    
+    </div>
+    <div class="popup-book-btn-add">
+      <button
+        type="button"
+        class="popup-book-btn-add-to-shopinglist"
+        data-id="${_id}"
+        id="add"
+        aria-label="Add card to shopping list"
+      ></button>
+    </div>
+
     `;
 }
 
 // ==============================================================
 // дописати логіку кнопки ADD TO SHOPPING LIST
 // ==============================================================
-// const addToShoppingListBtn = document.querySelector(
-//   '.popup-book-btn-add-to-shopinglist'
-// );
 
-// addToShoppingListBtn.addEventListener('click', addToShoppingListBtnHandler);
+function checkLocalStorage() {
+  const addToShoppingListBtn = document.querySelector(
+    '.popup-book-btn-add-to-shopinglist'
+  );
+  const bookId = addToShoppingListBtn.dataset.id;
 
-// function addToShoppingListBtnHandler() {
-//   console.log(
-//     "addToShoppingListBtnHandler"
-//   );
-// }
+  const localStorageData =
+    JSON.parse(localStorage.getItem('shoppinglist')) || [];
+
+  const bookInLocalStorage = localStorageData
+    .map(element => element._id)
+    .includes(bookId);
+
+  if (bookInLocalStorage) {
+    addToShoppingListBtn.textContent = 'REMOVE FROM THE SHOPPING LIST';
+    addToShoppingListBtn.insertAdjacentHTML('afterend', templateForBtn());
+  } else {
+    addToShoppingListBtn.textContent = 'ADD TO SHOPPING LIST';
+  }
+}
+
+function clickOnAddToShopingListBtn() {
+  const addToShoppingListBtn = document.querySelector(
+    '.popup-book-btn-add-to-shopinglist'
+  );
+  const bookId = addToShoppingListBtn.dataset.id;
+
+  addToShoppingListBtn.addEventListener('click', addToShoppingListBtnHandler);
+
+  function addToShoppingListBtnHandler() {
+    const localStorageData =
+      JSON.parse(localStorage.getItem('shoppinglist')) || [];
+
+    const bookInLocalStorage = localStorageData
+      .map(element => element._id)
+      .includes(bookId);
+
+    if (!bookInLocalStorage) {
+      saveBookToLocalStorage(bookId);
+      addToShoppingListBtn.textContent = 'REMOVE FROM THE SHOPPING LIST';
+      addToShoppingListBtn.insertAdjacentHTML('afterend', templateForBtn());
+    } else {
+      // Тут викликаэмо функцію видалення
+      document.querySelector('.under-btn-text').remove();
+      addToShoppingListBtn.textContent = 'ADD TO SHOPPING LIST';
+    }
+  }
+}
+
+function templateForBtn() {
+  return `
+      <p class="under-btn-text">
+        Congratulations! You have added the book to the shopping list. To
+        delete, press the button "Remove from the shopping list".
+      </p>
+    `;
+}
