e63fe5532dde3c71b3f853bb136f2f4082f43cd4
Uzurch1k <galcenkovlad7@gmail.com>
2024-03-05 19:22:59 +0200

    global fix

--- src/js/header.js
+++ src/js/header.js
@@ -6,7 +6,6 @@ const refs = {
   headerNav: document.querySelector('.header-menu'),
   headerLink: document.querySelectorAll('.header-nav-item-modal'),
 
-  logo: document.querySelector('.header-logo'),
   home: document.querySelector('.header-home'),
   shopping: document.querySelector('.header-shopping'),
   homeMobal: document.querySelector('.header-home-modal'),
@@ -25,35 +24,20 @@ refs.burger.addEventListener('click', handleHeaderLinkClick);
 
 // =========================== Home and Shopping ===========================
 
-function handleHomeClick() {
-  localStorage.setItem('isActive', 'home');
-}
-refs.logo.addEventListener('click', handleHomeClick);
-refs.home.addEventListener('click', handleHomeClick);
-refs.homeMobal.addEventListener('click', handleHomeClick);
-
-function handleShoppingClick() {
-  localStorage.setItem('isActive', 'shopping');
-}
-refs.shopping.addEventListener('click', handleShoppingClick);
-refs.shoppingMobal.addEventListener('click', handleShoppingClick);
-
-window.addEventListener('DOMContentLoaded', () => {
-  const activeElement = localStorage.getItem('isActive');
-  refs.home.classList.add('active');
-  refs.homeMobal.classList.add('active');
-
-  if (activeElement === 'home') {
+if (window.location.href.includes('index.html')) {
+  window.addEventListener('DOMContentLoaded', () => {
     refs.home.classList.add('active');
     refs.shopping.classList.remove('active');
 
     refs.homeMobal.classList.add('active');
     refs.shoppingMobal.classList.remove('active');
-  } else if (activeElement === 'shopping') {
+  });
+} else {
+  window.addEventListener('DOMContentLoaded', () => {
     refs.home.classList.remove('active');
     refs.shopping.classList.add('active');
 
     refs.homeMobal.classList.remove('active');
     refs.shoppingMobal.classList.add('active');
-  }
-});
+  });
+}
