24f872b699ffea416f86596eebad3258516603dc
Uzurch1k <galcenkovlad7@gmail.com>
2024-03-06 19:23:39 +0200

    fix firebase

--- src/js/authorization-functions.js
+++ src/js/authorization-functions.js
@@ -2,9 +2,14 @@
 
 const body = document.querySelector('body');
 const authen = document.querySelector('.authentication');
+
 const headerSubmit = document.querySelector('.header-btn-submit');
 const headerSubmitModal = document.querySelector('.header-btn-submit-modal');
+
 const authenClose = document.querySelector('.authentication-close');
+
+const signUpLink = document.querySelector('.authentication-signup');
+const signInLink = document.querySelector('.authentication-signin');
 
 // ==============================================================
 
@@ -13,20 +18,47 @@ function toggleAuthen(open) {
   body.classList.toggle('authentication-open', open);
 }
 
-if (headerSubmit) {
+// ==============================================================
+
+function showSignUpForm() {
+  const signUpForm = document.querySelector('.authentication-form-signup');
+  signUpForm.classList.remove('hide');
+  signUpLink.classList.add('active');
+
+  const signInForm = document.querySelector('.authentication-form-signin');
+  signInForm.classList.add('hide');
+  signInLink.classList.remove('active');
+}
+
+function showSignInForm() {
+  const signInForm = document.querySelector('.authentication-form-signin');
+  signInForm.classList.remove('hide');
+  signInLink.classList.add('active');
+
+  const signUpForm = document.querySelector('.authentication-form-signup');
+  signUpForm.classList.add('hide');
+  signUpLink.classList.remove('active');
+}
+
+if (authen) {
   headerSubmit.addEventListener('click', () => {
     toggleAuthen(true);
   });
-}
-if (headerSubmitModal) {
   headerSubmitModal.addEventListener('click', () => {
     toggleAuthen(true);
   });
-}
-if (authenClose) {
   authenClose.addEventListener('click', () => {
     toggleAuthen(false);
   });
-}
 
-// ==============================================================
+  // ===================================================
+
+  signUpLink.classList.add('active');
+
+  signUpLink.addEventListener('click', () => {
+    showSignUpForm();
+  });
+  signInLink.addEventListener('click', () => {
+    showSignInForm();
+  });
+}
--- src/js/authorization.js
+++ src/js/authorization.js
@@ -1,25 +1,80 @@
-// ==============================================================
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Инициализация Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyAVC9h8-zdX_Zwfa8u_SlIxJpF4-2QNWH4',
+  authDomain: 'bookshelf-pronto.firebaseapp.com',
+  projectId: 'bookshelf-pronto',
+  storageBucket: 'bookshelf-pronto.appspot.com',
+  messagingSenderId: '1040567130254',
+  appId: '1:1040567130254:web:b46de7db5d5f846b0f51df',
+  measurementId: 'G-7YPT2SGB8V',
+};
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAVC9h8-zdX_Zwfa8u_SlIxJpF4-2QNWH4',
-//   authDomain: 'bookshelf-pronto.firebaseapp.com',
-//   projectId: 'bookshelf-pronto',
-//   storageBucket: 'bookshelf-pronto.appspot.com',
-//   messagingSenderId: '1040567130254',
-//   appId: '1:1040567130254:web:b46de7db5d5f846b0f51df',
-//   measurementId: 'G-7YPT2SGB8V',
-// };
+initializeApp(firebaseConfig);
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// Получение ссылки на экземпляр аутентификации Firebase
+const auth = getAuth();
 
-// ==============================================================
+// Обработчик отправки формы регистрации
+const signUpForm = document.querySelector('.authentication-form-signup');
+if (signUpForm) {
+  signUpForm.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    const name = signUpForm.querySelector('#name-up').value;
+    const email = signUpForm.querySelector('#email-up').value;
+    const password = signUpForm.querySelector('#password-up').value;
+
+    try {
+      // Регистрация нового пользователя с использованием электронной почты, пароля и имени
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      // Установка имени пользователя
+      await updateProfile(userCredential.user, {
+        displayName: name,
+      });
+
+      // Дополнительные действия при успешной регистрации
+      console.log('User registered successfully:', userCredential.user);
+    } catch (error) {
+      // Обработка ошибок регистрации
+      console.error('Registration error:', error.message);
+    }
+  });
+}
+
+// Обработчик отправки формы входа
+const signInForm = document.querySelector('.authentication-form-signin');
+if (signInForm) {
+  signInForm.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    const email = signInForm.querySelector('#email-in').value;
+    const password = signInForm.querySelector('#password-in').value;
+
+    try {
+      // Вход пользователя с использованием электронной почты и пароля
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      // Дополнительные действия при успешном входе
+      console.log('User signed in successfully:', userCredential.user);
+    } catch (error) {
+      // Обработка ошибок входа
+      console.error('Sign in error:', error.message);
+    }
+  });
+}
