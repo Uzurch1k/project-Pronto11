862ea79aae42fb3376fa808392c73c1b2cd09736
Uzurch1k <galcenkovlad7@gmail.com>
2024-03-08 14:36:17 +0200

    fix elem

--- src/main.js
+++ src/main.js
@@ -1,23 +1,21 @@
 // ==============================================================
 
-import { toggle } from './js/feature';
+import * as feature from './js/feature';
 
-// import { fetchGeneral, fetchCategories, fetchCategory } from './js/fetch-api';
-// import { localStorage } from './js/local-storage';
+// import * as fetchApi from './js/fetch-api';
+// import * as localStorage from './js/local-storage';
 
 import * as header from './js/header';
 import * as authorization from './js/authorization';
 import * as authorizationFunctions from './js/authorization-functions';
 
-// import { categories } from './js/categories';
 import * as support from './js/support';
 
 import * as books from './js/books';
-// import { renderTop, renderCategories, renderCategory } from './js/rendering-books';
+// import * as renderingBooks from './js/rendering-books';
 
 import * as shopping from './js/shopping';
-// import { renderingBooks } from './js/rendering-shopping';
 // import * as pagination  from './js/pagination';
-// import './js/popup';
+// import * as popup './js/popup';
 
 // ==============================================================
